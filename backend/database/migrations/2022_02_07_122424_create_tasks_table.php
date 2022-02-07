8d9f7a173b87b07d43ee44a4020eb8d683323c79
Earl John Lester Bulasa <earl@elitech.digital>
2022-02-07 21:00:41 +0800

    create migration

---
+++ backend/database/migrations/2022_02_07_122424_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            //Serial datatype can't be found in laravel docss, will use increments instead
+            $table->increments('id');
+            $table->string('label');
+            $table->integer('sort_order');
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
